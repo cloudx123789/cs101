539c05e712f16b12a3de6254e6124aed59077be4
cloudx123789 <stephen0811@gmail.com>
2024-12-09 14:17:24 +0800

    main4.c

---
+++ hw5/main4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    int num=12345;
+    int ten_digits,hundred_digits,unit_digit,temp,after,thousand_digits,ten_thousand_digits;
+    int n [1000000];
+    if(num<=1000)
+    {
+        if(num/100>0)
+        {
+            hundred_digits=num/100;
+            temp=num%100;
+            ten_digits=temp/10;
+            unit_digit=temp%10;
+            after=1000*unit_digit+100*hundred_digits+10*ten_digits;
+            printf("%d",after);
+        }
+        else if (num/10>0)
+        {
+            ten_digits=num/10;
+            unit_digit=num%10;
+            after=1000*unit_digit+10*ten_digits;
+            printf("%d",after);
+        }
+        else
+        {
+            unit_digit=num;
+            after=1000*unit_digit;
+            printf("%d",after);
+        }
+    }
+    else
+    {
+        ten_thousand_digits=num/10000;
+        temp=num%10000;
+        thousand_digits=temp/1000;
+        temp=temp%1000;
+        hundred_digits=temp/100;
+        temp=temp%100;
+        ten_digits=temp/10;
+        unit_digit=temp%10;
+        after=10000*ten_thousand_digits+1000*unit_digit+100*hundred_digits+10*ten_digits+thousand_digits;
+        printf("%d",after);
+    }
+}
+
+       
