459b40eced9336b0d56f2b04806a35e0d94a61dc
cloudx123789 <stephen0811@gmail.com>
2024-12-12 16:00:44 +0800

    main2.c

--- EX4/main2.c
+++ EX4/main2.c
@@ -19,3 +19,28 @@ int main()
     printf("Pi:%.5f\n",p);
     return 0;
 }
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float n =1.0;
+    double p =0.0;
+    int i =0;
+
+    while(i<1000000)
+    {
+        p=p+(4.0/n*pow(-1, i));
+        n=n+2;
+        i=i+1;
+        long int ip=p*100000;
+        if(ip==314159)
+        {
+            break;   
+        }
+    }
+
+    printf("times:%d\n",n);
+    printf("Pi:%.5f\n",p);
+    return 0;
+}
