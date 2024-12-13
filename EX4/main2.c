c1fac84c4c418f52ca0684d83e2987e459a85057
cloudx123789 <stephen0811@gmail.com>
2024-12-13 18:46:08 +0800

    main2.c

--- EX4/main2.c
+++ EX4/main2.c
@@ -3,28 +3,7 @@
 
 int main()
 {
-    float n =1.0;
-    double p =0.0;
-    int i =0;
-    float t=0.000001; 
-
-    while (fabs(p-3.14159)>t)
-    {
-        p=p+(4.0/n*pow(-1, i));
-        n=n+2;
-        i=i+1;
-    }
-
-    printf("times:%d\n",i);
-    printf("Pi:%.5f\n",p);
-    return 0;
-}
-#include <stdio.h>
-#include <math.h>
-
-int main()
-{
-    float n =1.0;
+    int n =1;
     double p =0.0;
     int i =0;
 
