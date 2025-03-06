a7a098b42d2b2ed47345ff0ba65ab6b7307340df
cloudx123789 <stephen0811@gmail.com>
2025-03-07 00:25:12 +0800

    main1.c

--- HW#1/main1.c
+++ HW#1/main1.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-    char a[] = "AABBBCCCCddd";
+    char a[100];
     int count=0;
     int i=0;
-    while(i<sizeof(a))
+    scanf("%s",a);
+    while(a[i]!='\0')
     {
         if(a[i]==a[i+1])
         {
@@ -19,3 +20,21 @@ int main()
         i++;
     }
 }
+
+
+
+
+字元編碼
+Description
+
+設計一個演算法，檢查輸入進來的字元與其重複的數量，並依照順序輸出。
+
+
+Input
+
+使用 scanf() 函式接收輸入。
+
+
+Output
+
+使用printf() 輸出
